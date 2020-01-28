eed49ae642327be5e736cc0b14530ea999d17d80
Kristi Herin <kristi.chenault@gmail.com>
2020-01-27 19:45:53 -0500

    Made Page into a SPA

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
 $(document).ready(function(){
 
+//     $('#home').show()
+//   $('.aboutContent .projects .contact .resume').hide()
+$('#home').click(function(){
+    $('.aboutContent').hide()
+    $('.projects').hide()
+    $('.contact').hide()
+    $('.resume').hide()
     $('.homePage').show()
-  $('.about .projects .contact .resume').hide()
-  
-  $('.about').click(function(){
-      $('.about').show()
+})  
+
+  $('#aboutMe').click(function(){
+      $('.aboutContent').show()
       $('.projects').hide()
       $('.contact').hide()
       $('.resume').hide()
       $('.homePage').hide()
+      $('#selfie').show()
+      $('#goodreads').show()
   })
   
-  $('.projects').click(function(){
+  $('#projects').click(function(){
       $('.projects').show()
       $('.contact').hide()
       $('.resume').hide()
-      $('.about').hide()
+      $('.aboutContent').hide()
       $('.homePage').hide()
   })
   
-  $('.contact').click(function(){
+  $('#contact').click(function(){
       $('.contact').show()
       $('.resume').hide()
-      $('.about').hide()
+      $('.aboutContent').hide()
       $('.projects').hide()
       $('.homePage').hide()
   })
   
-  $('.resume').click(function(){
+  $('#resume').click(function(){
       $('.resume').show()
-      $('.about').hide()
+      $('.aboutContent').hide()
       $('.projects').hide()
       $('.contact').hide()
       $('.homePage').hide()
   })
-  
+
   
   }) //END READY FUNCTION
   
